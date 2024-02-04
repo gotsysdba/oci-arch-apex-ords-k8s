7ffdf958094bc3d216f356f06dc676f578eace01
John Lathouwers <john.lathouwers@oracle.com>
2024-02-04 23:10:51 +0000

    fix gateway logic

--- modules/network/locals.tf
+++ modules/network/locals.tf
@@ -2,9 +2,12 @@
 # All rights reserved. The Universal Permissive License (UPL), Version 1.0 as shown at http://oss.oracle.com/licenses/upl
 
 locals {
-  create_igw = var.byo_vcn ? length(data.oci_core_internet_gateways.igw.gateways) > 0 ? false : true : true
-  create_ngw = var.byo_vcn ? length(data.oci_core_nat_gateways.ngw.nat_gateways) > 0 ? false : true : true
-  create_sgw = var.byo_vcn ? length(data.oci_core_service_gateways.sgw.service_gateways) > 0 ? false : true : true
+  filtered_igw = [for igw in data.oci_core_internet_gateways.igw.gateways : igw.id if igw.display_name != format("%s-igw", var.label_prefix)]
+  create_igw = var.byo_vcn ? length(local.filtered_igw) > 0 ? false : true : true
+  filtered_ngw = [for ngw in data.oci_core_nat_gateways.ngw.nat_gateways : ngw.id if ngw.display_name != format("%s-ngw", var.label_prefix)]
+  create_ngw = var.byo_vcn ? length(local.filtered_ngw) > 0 ? false : true : true
+  filtered_sgw = [for sgw in data.oci_core_service_gateways.sgw.service_gateways : sgw.id if sgw.display_name != format("%s-sgw", var.label_prefix)]
+  create_sgw = var.byo_vcn ? length(local.filtered_sgw) > 0 ? false : true : true
 }
 
 locals {
